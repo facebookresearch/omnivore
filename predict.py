0842f5c82bd90f41648dfe9c13e682e1923d1c35
Ishan Misra <ishanmisra@gmail.com>
2022-02-01 11:40:43 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 """
 Download the weights in ./checkpoints and ImageNet 1K ID to class mappings beforehand
 wget https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json
