67149ce62248e517703548a86204124364b8d16d
Miguel Martin <miguelmartin@fb.com>
2022-03-10 22:34:45 -0800

    add setup.py and move to omnivore folder

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+
+from setuptools import find_packages, setup
+
+setup(
+    name="omnivore_fair",
+    version="1.0",
+    author="FAIR",
+    url="https://github.com/facebookresearch/omnivore",
+    install_requires=[],
+    tests_require=[],
+    packages=find_packages(),
+)
